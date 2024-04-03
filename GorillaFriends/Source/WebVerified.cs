77136be389f5de23b39170be899f832a3ea70b7d
Lightmaxifrvr <100568192+Lightmaxifrvr@users.noreply.github.com>
2024-04-03 17:15:42 +0200

    a

--- GorillaFriends/Source/WebVerified.cs
+++ GorillaFriends/Source/WebVerified.cs
@@ -5,7 +5,7 @@ namespace GorillaFriends
 {
     class WebVerified
     {
-        public const string m_szURL = "https://raw.githubusercontent.com/RusJJ/GorillaFriends/file_sources/gorillas.verified";
+        public const string m_szURL = "https://raw.githubusercontent.com/Lightmaxifrvr/GorillaFriends/main/file_sources/gorillas.verified";
         async public static void LoadListOfVerified()
         {
             HttpClient client = new HttpClient();
